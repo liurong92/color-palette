633a85803bb2c8fad2138aa3f5d39d4bf2af4398
rongliu <rongliu@thoughtworks.com>
2017-06-27 23:55:32 +0800

    update the color palette style and add name and color

--- app/scripts/colorPalette.js
+++ app/scripts/colorPalette.js
@@ -3,7 +3,12 @@ import React from 'react'
 class ColorPalette extends React.Component{
   render() {
     return (
-      <div>ColorPalette ddddliurong test test hahaha</div>
+      <div className='color-palette'>
+        <div className='color-palette-item' style={{background: 'red'}}>
+          <div className='color-palette-item-color'>#ddddd</div>
+          <div className='color-palette-item-name'>等等</div>
+        </div>
+      </div>
     )
   }
 }
