4274c0855cf7fdfc32773a54b803b546f96dc59b
rongliu <rongliu@thoughtworks.com>
2017-07-02 01:10:53 +0800

    add props type lib

---
+++ app/data.js
@@ -0,0 +1,18 @@
+export default [
+  { name: '浅粉红', color: '#FFB6C1' },
+  { name: '粉红', color: '#FFC0CB' },
+  { name: '深红(猩红)', color: '#DC143C' },
+  { name: '淡紫红', color: '#FFF0F5' },
+  { name: '弱紫罗兰红', color: '#DB7093' },
+  { name: '热情的粉红', color: '#FF69B4' },
+  { name: '深粉红', color: '#FF1493' },
+  { name: '中紫罗兰红', color: '#C71585' },
+  { name: '暗紫色(兰花紫)', color: '#DA70D6' },
+  { name: '蓟色', color: '#D8BFD8' },
+  { name: '洋李色(李子紫)', color: '#DDA0DD' },
+  { name: '紫罗兰', color: '#EE82EE' },
+  { name: '洋红(玫瑰红)', color: '#FF00FF' },
+  { name: '紫红(灯笼海棠)', color: '#FF00FF' },
+  { name: '深洋红', color: '#8B008B' },
+  { name: '紫色', color: '#800080' }
+]
--- app/scripts/colorPalette.js
+++ app/scripts/colorPalette.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import { PropTypes} from 'prop-types'
 
 class ColorPalette extends React.Component{
   render() {
+    const { data } = this.props
+
+    const elements = data.map( (item, index) => {
+      return (
+        <div key={index} className='color-palette-item' style={{background: item.color}}>
+          <div className='color-palette-item-color'>{ item.color }</div>
+          <div className='color-palette-item-name'>{ item.name }</div>
+        </div>
+      )
+    })
+
     return (
       <div className='color-palette'>
-        <div className='color-palette-item' style={{background: 'red'}}>
-          <div className='color-palette-item-color'>#ddddd</div>
-          <div className='color-palette-item-name'>等等</div>
-        </div>
+        {elements}
       </div>
     )
   }
+}
+
+ColorPalette.propTypes = {
+  data: PropTypes.array.isRequired
 }
 
 export default ColorPalette
