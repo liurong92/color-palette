390de559243bcbb9aeb567c5bc2af3d8dd46f9b0
rongliu <rongliu@thoughtworks.com>
2017-10-09 10:25:32 +0800

    remove some cofig

--- server.js
+++ server.js
@@ -6,13 +6,9 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
-  // It suppress error shown in console, so it has to be set to false.
   quiet: false,
-  // It suppress everything except error, so it has to be set to false as well
-  // to see success build.
   noInfo: false,
   stats: {
-    // Config for minimal console.log mess.
     assets: false,
     colors: true,
     version: false,
